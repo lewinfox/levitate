da840484bed6644667ae15476e164144cc11088e
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2021-02-04 08:30:27 +1300

    Add string coercion to string utils

--- R/string-utils.R
+++ R/string-utils.R
@@ -1,16 +1,33 @@
 #' Find all substrings of a given length
 #'
-#' @param x The input string.
+#' @param x The input string. Non-character inputs will be coerced with [as.character()].
 #' @param n The length of the desired substrings.
 #'
-#' @return A character vector containing all the length `n` substrings of `x`.
+#' @return A character vector containing all the length `n` substrings of `x`. If `x` has length >
+#'   then a list is returned containing an entry for each input element.
 #'
 #' @examples
 #' \dontrun{
 #' str_all_substrings("hello", 3)
 #' #> [1] "hel" "ell" "llo"
+#'
+#' str_all_substrings(rep("hello", 2), 4)
+#' #> [[1]]
+#' #> [1] "hell" "ello"
+#' #>
+#' #> [[2]]
+#' #> [1] "hell" "ello"
 #' }
 str_all_substrings <- function(x, n) {
+  if (!is.character(x)) {
+    x <- as.character(x)
+  }
+  if (length(x) > 1) {
+    return(lapply(x, str_all_substrings, n = n))
+  }
+  if (n < 1) {
+    rlang::abort("`n` must be a positive integer")
+  }
   unique(substring(x, 1:(nchar(x) - n + 1), n:nchar(x)))
 }
 
@@ -19,7 +36,7 @@ str_all_substrings <- function(x, n) {
 #' Splits an input string into tokens. A wrapper for [strsplit()] with a predefined split regex of
 #' `[^[:alnum:]]`, i.e. anything except `[a-zA-Z0-9]`.
 #'
-#' @param x The input string.
+#' @param x The input string. Non-character inputs will be coerced with [as.character()].
 #' @param split The regular expression to split on. See [strsplit()].
 #'
 #' @return A list containing one character vector for each element of `x`.
@@ -39,6 +56,9 @@ str_all_substrings <- function(x, n) {
 #' #> [1] "goodbye" "R"
 #' }
 str_tokenise <- function(x, split = "[^[:alnum:]]+") {
+  if (!is.character(x)) {
+    x <- as.character(x)
+  }
   # TODO: Is this the best regex to use? In the example given above, "isn't" splits into two.
   strsplit(x, split)
 }
@@ -48,7 +68,7 @@ str_tokenise <- function(x, split = "[^[:alnum:]]+") {
 #' Given an input string, tokenise it (using [str_tokenise()]), sort the tokens alphabetically and
 #' return the result as a single space-separated string.
 #'
-#' @param x The input string/s.
+#' @param x The input string. Non-character inputs will be coerced with [as.character()].
 #'
 #' @return A character vector the same length as `x` containing the sorted tokens.
 #'
@@ -61,6 +81,9 @@ str_tokenise <- function(x, split = "[^[:alnum:]]+") {
 #' #> [1] "fun great is R"                 "is less manipulation so string"
 #' }
 str_token_sort <- function(x) {
+  if (!is.character(x)) {
+    x <- as.character(x)
+  }
   tokens <- str_tokenise(x)
   sorted_tokens <- lapply(tokens, sort)
   sapply(sorted_tokens, paste, collapse = " ")
