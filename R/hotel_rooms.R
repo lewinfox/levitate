d9dd8ae13a657b01960f262d4a4744a81c6a56a1
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2021-04-10 22:18:01 +1200

    Add keyword internal to `hotel_rooms` docs
    To keep pkgdown happy

--- R/hotel_rooms.R
+++ R/hotel_rooms.R
@@ -10,5 +10,7 @@
 #'   \item{booking}{The title of the room's listing on booking.com}
 #' }
 #'
+#' @keywords internal
+#'
 #' @source Based on a dataset compiled by [Susan Li](https://github.com/susanli2016).
 "hotel_rooms"
