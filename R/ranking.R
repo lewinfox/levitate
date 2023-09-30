2fe38842a902b167efff20e54d59409ec88702a5
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2023-09-30 16:36:58 +1300

    Rename ranking functions to `lev_*` for better consistency

--- R/ranking.R
+++ R/ranking.R
@@ -15,10 +15,10 @@
 #'   according to the `decreasing` parameter, so by default higher scores are first.
 #'
 #' @examples
-#' score_multiple("bilbo", c("frodo", "gandalf", "legolas"))
+#' lev_score_multiple("bilbo", c("frodo", "gandalf", "legolas"))
 #' @export
-#' @seealso [best_match()]
-score_multiple <- function(input, candidates, .fn = lev_ratio, ..., decreasing = TRUE) {
+#' @seealso [lev_best_match()]
+lev_score_multiple <- function(input, candidates, .fn = lev_ratio, ..., decreasing = TRUE) {
   if (length(input) > 1) rlang::abort(glue::glue("`input` must be length 1, not {length(input)}"))
   .fn <- match.fun(.fn)
   scores <- sort(sapply(candidates, .fn, input, ...), decreasing = decreasing)
@@ -30,13 +30,13 @@ score_multiple <- function(input, candidates, .fn = lev_ratio, ..., decreasing =
 #' Given an `input` string and multiple `candidates`, return the candidate with the best score as
 #' calculated by `.fn`.
 #'
-#' @inheritParams score_multiple
+#' @inheritParams lev_score_multiple
 #' @return A string
-#' @seealso [score_multiple()]
+#' @seealso [lev_score_multiple()]
 #' @examples
-#' best_match("bilbo", c("frodo", "gandalf", "legolas"))
+#' lev_best_match("bilbo", c("frodo", "gandalf", "legolas"))
 #' @export
-best_match <- function(input, candidates, .fn = lev_ratio, ..., decreasing = TRUE) {
-  scores <- score_multiple(input = input, candidates = candidates, .fn = .fn, ..., decreasing = decreasing)
+lev_best_match <- function(input, candidates, .fn = lev_ratio, ..., decreasing = TRUE) {
+  scores <- lev_score_multiple(input = input, candidates = candidates, .fn = .fn, ..., decreasing = decreasing)
   names(scores)[[1]]
 }
--- tests/testthat/test-ranking.R
+++ tests/testthat/test-ranking.R
@@ -1,14 +1,14 @@
-test_that("`score_multiple()` works", {
-  res <- score_multiple("bilbo", c("frodo", "gandalf", "legolas"))
+test_that("`lev_score_multiple()` works", {
+  res <- lev_score_multiple("bilbo", c("frodo", "gandalf", "legolas"))
   expect_true(is.list(res))
   expect_length(res, 3)
 })
 
-test_that("`best_match()` works", {
-  res <- best_match("bilbo", c("frodo", "gandalf", "legolas"))
+test_that("`lev_best_match()` works", {
+  res <- lev_best_match("bilbo", c("frodo", "gandalf", "legolas"))
   expect_length(res, 1)
   expect_equal(res, "frodo")
 
-  res_2 <- best_match("bilbo", c("frodo", "gandalf", "legolas"), decreasing = FALSE)
+  res_2 <- lev_best_match("bilbo", c("frodo", "gandalf", "legolas"), decreasing = FALSE)
   expect_equal(res_2, "gandalf")
 })
