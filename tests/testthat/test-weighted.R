f5f109fd703aa467c4df8b0126a2c7229d1a3984
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2023-10-02 15:04:07 +1300

    Add additional tests for weighted functions

--- tests/testthat/test-weighted.R
+++ tests/testthat/test-weighted.R
@@ -31,3 +31,26 @@ test_that("`lev_weighted_token_ratio()` handles zero-weighted tokens correctly",
     lev_weighted_token_ratio("bill", "jill")
   )
 })
+
+test_that("weighted functions score identical strings as 1", {
+  expect_equal(lev_weighted_token_ratio("abc def", "abc def"), 1)
+  expect_equal(lev_weighted_token_set_ratio("abc def", "abc def"), 1)
+  expect_equal(lev_weighted_token_set_ratio("abc def", "abc def"), 1)
+})
+
+test_that("weighted functions score entirely dissimilar strings as 0", {
+  expect_equal(lev_weighted_token_ratio("abc def", "ghi jkl"), 0)
+  expect_equal(lev_weighted_token_set_ratio("abc def", "ghi jkl"), 0)
+  expect_equal(lev_weighted_token_set_ratio("abc def", "ghi jkl"), 0)
+})
+
+test_that("weighted function outputs are always between 0 and 1", {
+  between_0_and_1 <- function(x) x >= 0 && x <= 1
+  for (i in seq(1, 50)) {
+    random_para_a <- stringi::stri_rand_lipsum(1)
+    random_para_b <- stringi::stri_rand_lipsum(1)
+    for (fn in c(lev_weighted_token_ratio, lev_weighted_token_sort_ratio, lev_weighted_token_set_ratio)) {
+      expect_true(between_0_and_1(fn(random_para_a, random_para_b)))
+    }
+  }
+})
